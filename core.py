138ab727b802a606f9432ad93b81ecb4adbfba21
Guilherme Kuhn <g.kuhn0@gmail.com>
2012-04-20 19:26:21 -0300

    First Commit

---
+++ core.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+
+class P2py(object):
+
+    def __init__(self, *args, **kwargs):
+        '''
+        '''
---
+++ files/models.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+from django.db import models
+
+class Index(models.Model):
+
+    ip = models.CharField('IP',
+                          max_length=15)
+    filename = models.CharField('Filename',
+                            max_length=1000)
+    size = models.CharField('Size'
+                            max_length=20)
+
+    def __unicode__(self):
+        return self.filename
